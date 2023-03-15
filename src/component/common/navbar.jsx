c7ea752224a2f9bd626bcc95a11e85502f02d7b6
Keshav Prakash Lad <ladsagar666@gmail.com>
2023-03-15 15:28:39 +0530

    All Tables Added

--- src/component/common/navbar.jsx
+++ src/component/common/navbar.jsx
@@ -3,7 +3,11 @@ import "./Css/navbar.css";
 import Notification from "../../utils/icons/Notification.svg";
 import Newuser from "../../utils/images/Newuser.png";
 
+
 const Navbar=()=>{
+
+
+  
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm ">
       <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/utils/apicalls/get.jsx
+++ src/utils/apicalls/get.jsx
@@ -1,3 +1,28 @@
 import React from "react";
+import axios from "axios";
 
-const Get
+const GetData = () => {
+    axios
+    .get("https://jsonplaceholder.typicode.com/customers")
+    .then(data => console.log(data.data))
+    .catch(error => console.log(error));
+    };
+export default GetData;
+
+// const Getclient=()=>{
+//     const defaultOptions={
+//         baseURL: "https://dummyjson.com/",
+//         method: 'get',
+//         headers:{
+//             'Content-type':'application/json'
+//         }
+//     };
+//     let instance=axios.create(defaultOptions);
+//     instance.interceptors.request.use(function(config){
+//         // config.headers['api-id']='';
+//         return config;
+//     });
+//     return instance;
+//     // console.log(config)
+// };
+// export default Getclient();
